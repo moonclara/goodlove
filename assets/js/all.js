01e6d9a1198e60dfae74acad24a8d687653dc07c
Clara <smile629639@gmail.com>
2022-09-18 07:49:36 +0800

    Update 2022-09-17T23:49:25.579Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,16 +1,14 @@
-$(function () {
-  console.log('Hello Bootstrap5');
+"use strict";
 
-  //購物說明
+$(function () {
+  console.log('Hello Bootstrap5'); //購物說明
 
   $('.shop-direction .collapse').on('shown.bs.collapse', function () {
     $(this).parent().find(".fa-chevron-down").removeClass("fa-chevron-down").addClass("fa-chevron-up");
   }).on('hidden.bs.collapse', function () {
     $(this).parent().find(".fa-chevron-up").removeClass("fa-chevron-up").addClass("fa-chevron-down");
-  });
+  }); //AOS
 
-  //AOS
   AOS.init();
-
- 
 });
+//# sourceMappingURL=all.js.map
